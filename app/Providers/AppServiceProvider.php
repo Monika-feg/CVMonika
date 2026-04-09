b413633fe2fc06a09768fba9f4f05bb80e5e2381
monika eengström <monika@MSI.>
2026-04-09 14:36:34 +0200

    problem med att den inte hittar rätt ULR så har ändrat i appserviceprovider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        if (app()->environment('production')) {
+            \URL::forceScheme('https');
+        }
         $this->configureDefaults();
     }
 
